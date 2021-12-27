20a6f707dc4fdb0e5b5af75fa0292a330e8df57a
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2021-12-27 12:39:47 +0330

    update: add live data for animal list

--- app/src/main/java/com/naji/funnyAnimals/ui/screen/WildAnimalViewModel.kt
+++ app/src/main/java/com/naji/funnyAnimals/ui/screen/WildAnimalViewModel.kt
@@ -1,6 +1,35 @@
 package com.naji.funnyAnimals.ui.screen
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.naji.funnyAnimals.data.Animal
+import com.naji.funnyAnimals.data.AnimalData
 
 class WildAnimalViewModel : ViewModel() {
+
+    fun init(){
+        updateAnimals(AnimalData.AnimalSample)
+    }
+
+    private val animalLiveData = MutableLiveData<List<Animal>>()
+
+    fun getAnimalList(): MutableLiveData<List<Animal>> {
+        return animalLiveData
+    }
+
+    private fun updateAnimals(animals:List<Animal>){
+        animalLiveData.value = animals
+    }
+
+
+    fun clickOnItem(animal: Animal){
+        val animalList =AnimalData.AnimalSample
+        for(it in animalList){
+            if(it!=animal)
+                it.animate=false
+            else
+                it.animate=animal.animate
+        }
+        updateAnimals(animalList)
+    }
 }
