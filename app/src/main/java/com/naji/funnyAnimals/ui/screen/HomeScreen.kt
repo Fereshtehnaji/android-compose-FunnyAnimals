904451f3c4ff8f4e9aba5bbb868483523970a964
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2021-12-27 12:40:56 +0330

    update: change design home page

--- app/src/main/java/com/naji/funnyAnimals/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/naji/funnyAnimals/ui/screen/HomeScreen.kt
@@ -1,25 +1,165 @@
 package com.naji.funnyAnimals.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.naji.funnyAnimals.R
+import com.naji.funnyAnimals.data.CardHome
+import com.naji.funnyAnimals.data.HomeCardData
+import com.naji.funnyAnimals.ui.theme.Black
+import com.naji.funnyAnimals.ui.theme.Green500
+import com.naji.funnyAnimals.ui.theme.Orange50
+import com.naji.funnyAnimals.ui.theme.Orange500
+
 
 @Composable
-fun HomeScreen(navController: NavController){
-    Column(modifier = Modifier.padding(30.dp)) {
-
-        Button(onClick = {
-            navController.navigate("DomesticAnimal") }) {
-            Text(text = "حیوانات اهلی")
-        }
-
-        Button(onClick = { navController.navigate("WildAnimal") }) {
-            Text(text = "حیوانات وحشی")
-        }
+fun HomeScreen(navController: NavController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(30.dp)
+            .fillMaxSize()
+    ) {
+        Header()
+        Spacer(modifier = Modifier.height(24.dp))
+        GridHome(HomeCardData.HomeCardList, navController = navController)
     }
 }
+
+@Composable
+fun GridHome(cardList: List<CardHome>, navController: NavController) {
+
+    LazyColumn() {
+        items(items = cardList.chunked(2)) { rowItems ->
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(horizontal = 8.dp),
+            )
+            {
+                for (item in rowItems) {
+                    Box(
+
+                        modifier = Modifier
+                            .animateContentSize()
+                            .padding(1.dp)
+                            .weight(1f)
+                            .aspectRatio(1f)
+                            .background(
+                                color = item.background,
+                                shape = MaterialTheme.shapes.medium
+                            ),
+                        Alignment.Center
+                    ) {
+
+                        HomeCard(item, navController)
+                    }
+
+                }
+            }
+            Spacer(Modifier.height(8.dp))
+        }
+
+    }
+}
+
+@Composable
+fun Header() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(all = 8.dp)
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.animal_lion),
+            contentDescription = stringResource(R.string.content_description_main_icon),
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+                .background(Orange50)
+                .padding(all = 6.dp),
+
+
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.home_title),
+            color = Black,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.defaultMinSize(50.dp),
+            textAlign = TextAlign.Center,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = stringResource(R.string.home_body),
+            color = Orange500,
+            style = MaterialTheme.typography.body2,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+
+        )
+    }
+}
+
+@Composable
+fun HomeCard(cardHome: CardHome, navController: NavController) {
+
+    val verySmallPadding = dimensionResource(R.dimen._4sdp)
+    val smallPadding = dimensionResource(R.dimen._4sdp)
+    val mediumPadding = dimensionResource(R.dimen._4sdp)
+    val largePadding = dimensionResource(R.dimen._4sdp)
+    val imageSize = dimensionResource(R.dimen._40sdp)
+
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(all = mediumPadding)
+            .clickable { navController.navigate(cardHome.route) }
+    ) {
+        Image(
+            painter = painterResource(id = cardHome.icon),
+            contentDescription = "this is sample picture",
+            modifier = Modifier
+                .size(imageSize)
+                .clip(CircleShape)
+                .background(cardHome.backgroundIcon)
+                .padding(verySmallPadding)
+        )
+        Spacer(modifier = Modifier.height(largePadding))
+
+        Text(
+            text = cardHome.title,
+            color = cardHome.titleColor,
+            style = MaterialTheme.typography.subtitle2,
+            textAlign = TextAlign.Right,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = cardHome.description,
+            Modifier.padding(all = smallPadding),
+            maxLines = 1,
+            style = MaterialTheme.typography.overline,
+
+            )
+    }
+}
+
