597142da81699ce80e7cf09c6a8c3d9958db59d5
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2021-12-27 12:39:10 +0330

    update: add title color to object home card

--- app/src/main/java/com/naji/funnyAnimals/data/HomeCardData.kt
+++ app/src/main/java/com/naji/funnyAnimals/data/HomeCardData.kt
@@ -8,6 +8,7 @@ data class CardHome(
     val title: String, val description: String, val icon: Int,
     val background: Color,
     val backgroundIcon: Color,
+    val titleColor: Color,
     val route: String
 )
 
@@ -15,19 +16,19 @@ object HomeCardData {
     val HomeCardList = listOf(
         CardHome(
             "حیوانات", "آشنایی با حیوانات",
-            R.drawable.animal_sheep, Green50, Green100, "DomesticAnimal"
+            R.drawable.animal_sheep, Green50, Green100, Green500,"DomesticAnimal"
         ),
         CardHome(
             "پرندگان", "آشنایی با پرندگان",
-            R.drawable.bird_parrot, Orange50, Orange100, "WildAnimal"
+            R.drawable.bird_parrot, Orange50, Orange100,Orange500, "WildAnimal"
         ),
         CardHome(
             "حشرات", "آشنایی با حشرات",
-            R.drawable.animal_leopard, Orange50, Orange100, "WildAnimal"
+            R.drawable.animal_leopard, Orange50, Orange100,Orange500, "WildAnimal"
         ),
         CardHome(
             "دریایی", "آشنایی با آبزیان",
-            R.drawable.animal_fish, Cyan50, Cyan100, "WildAnimal"
+            R.drawable.animal_fish, Cyan50, Cyan100,Cyan500, "WildAnimal"
         )
     )
 
