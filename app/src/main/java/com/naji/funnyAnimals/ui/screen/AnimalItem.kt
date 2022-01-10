f0dd75409979cb160439d12b4c147ca91a8430af
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2022-01-10 08:55:00 +0330

    UPDATE: remove ripple effect on item click

--- app/src/main/java/com/naji/funnyAnimals/ui/screen/AnimalItem.kt
+++ app/src/main/java/com/naji/funnyAnimals/ui/screen/AnimalItem.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -57,7 +59,8 @@ fun ShowImage(animal: Animal, onClickHandler: (Animal) -> Unit) {
             contentDescription = "this is sample picture",
             modifier = Modifier
                 .size(imageSize)
-                .clickable() {
+                .clickable(   interactionSource = remember { MutableInteractionSource() },
+                    indication = null) {
                     stopSound(context = context)
                     playSound(context = context, sound = animal.sound)
                     onClickHandler(animal)
