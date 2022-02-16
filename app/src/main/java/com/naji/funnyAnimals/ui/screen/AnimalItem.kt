5b7d4a20331490352723a4e4c109b1bf3b326fca
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2022-02-16 22:24:14 +0330

    REFACTOR: arrange

--- app/src/main/java/com/naji/funnyAnimals/ui/screen/AnimalItem.kt
+++ app/src/main/java/com/naji/funnyAnimals/ui/screen/AnimalItem.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,12 +26,15 @@ import androidx.compose.ui.unit.dp
 import com.naji.funnyAnimals.R
 import com.naji.funnyAnimals.data.Animal
 import com.naji.funnyAnimals.data.animalenum.TYPE
-import com.naji.funnyAnimals.ui.theme.*
+import com.naji.funnyAnimals.ui.theme.Cyan200
+import com.naji.funnyAnimals.ui.theme.Green200
+import com.naji.funnyAnimals.ui.theme.Orange200
+import com.naji.funnyAnimals.ui.theme.Yellow300
 import com.naji.funnyAnimals.ui.util.MusicManager
 
 
 @Composable
-fun AnimalItem(animal: Animal, onClickHandler: (Animal) -> Unit,type:TYPE) {
+fun AnimalItem(animal: Animal, onClickHandler: (Animal) -> Unit, type: TYPE) {
 
     val context = LocalContext.current
 
@@ -50,7 +54,7 @@ fun AnimalItem(animal: Animal, onClickHandler: (Animal) -> Unit,type:TYPE) {
                 }
         ) {
 
-            ItemAnimation(animal.shape, animal.isClicked,type)
+            ItemAnimation(animal.shape, animal.isClicked, type)
 
             ShowLabel(animal.label, visibility = animal.isClicked)
         }
@@ -58,16 +62,16 @@ fun AnimalItem(animal: Animal, onClickHandler: (Animal) -> Unit,type:TYPE) {
 }
 
 @Composable
-fun ItemAnimation(image: Int, isClicked: Boolean,type:TYPE) {
+fun ItemAnimation(image: Int, isClicked: Boolean, type: TYPE) {
 
     val animationDegree = getRotateAnimationDegree(isClicked)
     val scale = getScaleAnimation(isClicked)
 
-    ShowImage(image, animationDegree, scale,type)
+    ShowImage(image, animationDegree, scale, type)
 }
 
 @Composable
-fun ShowImage(image: Int, rotationDegrees: Float = 0f, scaleValue: Float = 0f,type:TYPE) {
+fun ShowImage(image: Int, rotationDegrees: Float = 0f, scaleValue: Float = 0f, type: TYPE) {
 
     val imageSize = dimensionResource(id = R.dimen._60sdp)
 
@@ -87,13 +91,13 @@ fun ShowImage(image: Int, rotationDegrees: Float = 0f, scaleValue: Float = 0f,ty
 }
 
 @Composable
-fun colorBackground(type:TYPE) : Color{
-   return when (type){
+fun colorBackground(type: TYPE): Color {
+    return when (type) {
         TYPE.ANIMAL -> Green200
         TYPE.BUG -> Yellow300
         TYPE.BIRD -> Orange200
         TYPE.AQUATIC -> Cyan200
-   }
+    }
 }
 
 
@@ -107,29 +111,20 @@ fun ShowLabel(label: Int, visibility: Boolean) {
             painter = painterResource(label),
             contentDescription = "",
             modifier = Modifier
-                .size(imageSize),alignment = Alignment.BottomCenter
+                .size(imageSize), alignment = Alignment.BottomCenter
 
         )
 }
 
 @Composable
 fun getScaleAnimation(isClicked: Boolean): Float {
+
     val clickedAnimation by animateFloatAsState(
         targetValue = if (isClicked) 1.5F else 1f,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioHighBouncy,
             stiffness = Spring.StiffnessVeryLow
         )
-
-//                animationSpec = repeatable(
-//            iterations = 1,
-//            animation = tween(
-//                durationMillis = 500,
-//                easing = FastOutSlowInEasing
-//            ),
-//            repeatMode = RepeatMode.Reverse
-//        ), finishedListener = {
-//        }
     )
     return clickedAnimation
 }
@@ -137,30 +132,20 @@ fun getScaleAnimation(isClicked: Boolean): Float {
 @Composable
 fun getRotateAnimationDegree(isClicked: Boolean): Float {
     val infiniteTransition = rememberInfiniteTransition()
+    val hesitateEasing = CubicBezierEasing(0.2f, 0.2f, 0.8F, 0.8f)
 
     val rotateInDegreeAnimation by infiniteTransition.animateFloat(
-        initialValue = 0F,
-        targetValue = 40F,
-        animationSpec = infiniteRepeatable(
-            animation = tween(
-                durationMillis = 1500,
-                easing = FastOutSlowInEasing
-            ),
+
+        initialValue = -20F,
+        targetValue = 20F,
+        animationSpec = InfiniteRepeatableSpec(
+            animation = tween(1000, easing = hesitateEasing),
             repeatMode = RepeatMode.Reverse
         )
     )
 
-//    val rotateAnim by animateFloatAsState(
-//        targetValue = if (isClicked) 360F else 0f,
-//        animationSpec = tween(durationMillis = 2500),
-//        finishedListener = {
-//        }
-
-//    )
-
     return if (isClicked) rotateInDegreeAnimation else 0f
 }
-
 
 
 fun playSound(context: Context, sound: Int) {
